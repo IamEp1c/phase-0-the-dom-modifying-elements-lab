403841156440f3fdaf3e84d69e6a39d41205e910
Waqas Anjum <Qanjum96@gmail.com>
2022-04-11 18:03:47 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.querySelector('#main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'        // not a function , do not invoke like a function 
+
+newHeader.textContent = 'Qaz is the champion'
+
+
+
+
